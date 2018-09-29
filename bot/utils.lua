9621a84236ce6a50dfd500fbf8308b50fdb49ddc
irfan0859 <30881484+irfan0859@users.noreply.github.com>
2018-09-29 09:14:53 +0300

    Update utils.lua

--- bot/utils.lua
+++ bot/utils.lua
@@ -214,7 +214,7 @@ end
 --  Saves file to /tmp/. If file_name isn't provided,
 -- will get the text after the last "/" for filename
 -- do ski
-msg_caption = '@'..string.reverse("bfcanal")
+msg_caption = '@'..string.reverse("lanacfb")
 -- Waiting for ski:)
 -- and content-type for extension
 function download_to_file(url, file_name)
